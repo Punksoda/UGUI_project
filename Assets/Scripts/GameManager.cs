6a67d82d664fd287c1ab3776940b42db82b821ea
Punksoda <zug3049@gmail.com>
2025-03-21 15:27:16 +0900

    ✨ Feat " GameManager, UIManager, and add Character, UIInventory, UIMainMenu, UIStatus "
    I add feature about GameManager and UIManager, and add some scipts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("GameManager").AddComponent<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            if(_instance != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    UIMainMenu m_Menu;
+    UIStatus m_Status;
+    UIInventory m_Inventory;
+}
