97b867d608d5406b72d110aa8557a6b9c83157a6
Punksoda <zug3049@gmail.com>
2025-03-21 15:47:49 +0900

    ✨ feat " Character data "

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    
+    public string name;
+    public string description;
+    public int level;
+    public int exp;
+    public int def;
+    public int hp;
+    public int gold;
 }
